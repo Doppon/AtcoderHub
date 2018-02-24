64812a4ff7022b66f66e60a0b50d6aa3b08b1369
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-02-25 00:55:53 +0900

    作成コマンドの追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,16 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all
+    @newTopic = Topic.new
   end
 
   def show
   end
+
+  def create
+    @topic = Topic.new(params[:topic].permit(:title))
+    @topic.save
+    redirect_to topics_index_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'topics/show/:id' => 'topics#show', as: :topics_show
 
+  post 'topics/create' => 'topics#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
