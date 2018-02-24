8c1737d02b0c75128c96e3ebd4fdf72a85bebfa0
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-02-25 00:45:31 +0900

    一覧画面の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'topics/index'
 
-  get 'topics/show'
+  get 'topics/show/:id' => 'topics#show', as: :topics_show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
