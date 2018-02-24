305f40353a8ef31cd4f3b81e24219330e4855af2
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-02-25 01:09:56 +0900

    deleteコマンドの追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,4 +13,9 @@ class TopicsController < ApplicationController
     redirect_to topics_index_path
   end
   
+  def delete
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   post 'topics/create' => 'topics#create'
 
+  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
