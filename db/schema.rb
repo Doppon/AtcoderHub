34a22a31ed8479ef596b56cedb3ba7b65fcc0d9f
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-14 01:15:51 +0900

    スキーマを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224162136) do
+ActiveRecord::Schema.define(version: 2018_02_24_162136) do
 
-  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "body"
-    t.bigint "topic_id"
+    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
-  create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "topics"
 end
