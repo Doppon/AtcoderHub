22a2e4917de47e5b2c824e382f70ced84ae07170
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-21 23:10:43 +0900

    コントローラーごとにルーティングをリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
   end
 
   root to: 'home#index'
+  
   get 'home/index'
+
   get 'topics/index'
   get 'topics/new'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
   post 'topics/create' => 'topics#create'
   delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
+  
   post 'posts/create' => 'post#create', as: :post_create
   delete 'posts/delete/:id' => 'post#delete', as: :post_delete
 end
