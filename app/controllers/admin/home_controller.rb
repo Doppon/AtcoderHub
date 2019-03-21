bae3deeabac424bd32931ac9fc885a43c3c62559
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-21 22:40:56 +0900

    管理画面側にBasic認証を追加

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,14 @@
 class Admin::HomeController < ApplicationController
+  before_action :basic
+
   def index
+  end
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      # name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      name == "name" && password == "pass"
+    end
   end
 end
