75c832f7363874f46323088a509b4c5712e72bab
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-21 14:11:46 +0900

    無駄な改行を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
   root to: 'topics#index'
-
   get 'topics/index'
-
   get 'topics/show/:id' => 'topics#show', as: :topics_show
-
   post 'topics/create' => 'topics#create'
-
   delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
-
   post 'posts/create' => 'post#create', as: :post_create
-
   delete 'posts/delete/:id' => 'post#delete', as: :post_delete
 end
