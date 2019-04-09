e151bc842a04f186e59fa7cf76f4041507e5d267
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-10 00:26:27 +0900

    パスワードの暗号化に伴いカラム名をpassword_digestに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_150252) do
+ActiveRecord::Schema.define(version: 2019_04_09_152216) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_04_09_150252) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
