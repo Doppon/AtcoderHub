7d776d2149d7c8e08ff5e30f7ae67e6bf2c25555
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-10 00:07:03 +0900

    ユーザーモデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   namespace :admin do
     root to: 'home#index'
     get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_24_162136) do
+ActiveRecord::Schema.define(version: 2019_04_09_150252) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_02_24_162136) do
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
