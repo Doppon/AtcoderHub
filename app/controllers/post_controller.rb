ef14fcdb523552fdb409343c804ebae8795295fd
Doppon <zaxpkuraudo7777@gmail.com>
2019-06-08 17:18:17 +0900

    ToDoの追加

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -8,6 +8,7 @@ class PostController < ApplicationController
   def delete
     @post = Post.find(params[:id])
     @post.delete
+    # ToDo: リダイレクト先を会話している詳細画面に遷移させたい
     redirect_to home_index_path
   end
 end
