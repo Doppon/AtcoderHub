ee0dd106a1941ea591f69b1a61da2f7f24acc3fe
Doppon <zaxpkuraudo7777@gmail.com>
2019-06-08 17:10:52 +0900

    トピック部分にもベーシック認証を行うように修正

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -8,7 +8,7 @@ class Admin::HomeController < ApplicationController
 
   def basic
     authenticate_or_request_with_http_basic do |name, password|
-      # ToDo: 呼び出し場所を修正
+      # ToDo: 呼び出し場所を修正したい
       require 'dotenv'
       Dotenv.load
       name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -1,5 +1,19 @@
 class Admin::TopicsController < ApplicationController
+  # ToDo: BASIC認証を管理画面全てに適応したい
+  before_action :basic
+
   def index
     @topics = Topic.all
+  end
+
+  private
+
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      # ToDo: 呼び出し場所を修正したい
+      require 'dotenv'
+      Dotenv.load
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
