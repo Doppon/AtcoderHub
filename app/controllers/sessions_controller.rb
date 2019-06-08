2a24d33aa69911114887044fff2e410aa3843ce0
Doppon <zaxpkuraudo7777@gmail.com>
2019-06-08 18:38:36 +0900

    ログイン処理を行うcreateアクションを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: session_params[:name])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:name, :password)
+  end
 end
