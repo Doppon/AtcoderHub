fc859f2cfa46d364fc10706f467abf4acb47c8a5
Doppon <zaxpkuraudo7777@gmail.com>
2019-06-20 12:31:38 +0900

    admin側にApplicationControllerを作る事で致命的なセキュリティーホールを解消

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,19 +1,4 @@
 class Admin::HomeController < ApplicationController
-  # ToDo: BASIC認証を管理画面全てに適応したい
-  # => helper_method を用いて解決できそう
-  before_action :basic
-
   def index
-  end
-
-  private
-
-  def basic
-    authenticate_or_request_with_http_basic do |name, password|
-      # ToDo: 呼び出し場所を修正したい
-      require 'dotenv'
-      Dotenv.load
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
   end
 end
--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -1,20 +1,5 @@
 class Admin::TopicsController < ApplicationController
-  # ToDo: BASIC認証を管理画面全てに適応したい
-  # => helper_method を用いて解決できそう
-  before_action :basic
-
   def index
     @topics = Topic.all
-  end
-
-  private
-
-  def basic
-    authenticate_or_request_with_http_basic do |name, password|
-      # ToDo: 呼び出し場所を修正したい
-      require 'dotenv'
-      Dotenv.load
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
   end
 end
