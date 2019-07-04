0a7bbbac2d5d8dabfeb3c9eaa52632c1e540871e
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-04 11:29:11 +0900

    deleteメソッドを追加

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -2,4 +2,10 @@ class Admin::TopicsController < Admin::ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def delete
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to home_index_path
+  end
 end
