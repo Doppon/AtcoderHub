79f8f52861e9b3cf361e66bd3b29c23e296de29f
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-04 11:36:50 +0900

    ToDoの追加

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -1,3 +1,4 @@
+# ToDo: Admin側のViewからこっちのコントローラーをみてくれていない
 class Admin::TopicsController < Admin::ApplicationController
   def index
     @topics = Topic.all
@@ -6,6 +7,6 @@ class Admin::TopicsController < Admin::ApplicationController
   def delete
     @topic = Topic.find(params[:id])
     @topic.destroy
-    redirect_to home_index_path
+    redirect_to admin_topics_index_path
   end
 end
